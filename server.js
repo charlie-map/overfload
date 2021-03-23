f36099b1db6552c0dc3fa0fc4664f58f50a15874
charlie-map <charjhall777@gmail.com>
2021-03-23 00:15:57 -0400

    my last commit!

--- server.js
+++ server.js
@@ -3,6 +3,10 @@ require('dotenv').config({
 });
 const AWS = require('aws-sdk');
 const express = require('express');
+const Busboy = require('busboy');
+const {
+  v4: uuidv4
+} = require('uuid');
 const app = express();
 // configure sdk for use with DO Spaces
 const endpoint = new AWS.Endpoint(process.env.SPACES_ENDPOINT);
@@ -12,16 +16,43 @@ const space = new AWS.S3({
   secretAccessKey: process.env.SPACES_SECRET,
 });
 
-app.get('/gimme-link', (req, res) => {
-  async (file, key) => {
-    return space
-      .putObject({
-        Body: file.data,
-        Bucket: process.env.SPACES_BUCKET,
-        Key: key,
-        ACL: 'public-read',
-        ContentType: file.mimetype,
-      })
-      .promise();
-  };
+const maidboy = function(req, res, next) {
+  let bb = new Busboy({
+    headers: req.headers
+  });
+  let the_dump = uuidv4();
+  bb.on('file', function(fieldname, file, filename, encoding, mimetype) {
+    if (mimetype.match(/image\/[A-Za-z]/g)) {
+      space
+        .upload({
+          Body: file,
+          Bucket: process.env.SPACES_BUCKET,
+          Key: the_dump,
+          ACL: 'public-read',
+          ContentType: mimetype,
+        }, (err, data) => {
+          if (err) console.log("Oopsy Doopsy", err);
+          console.log("woo", data);
+        });
+    } else {
+      next("Oopsy doopsy! Kiro wanted a boy!");
+    }
+  });
+  bb.on('finish', function() {
+    req.url_cute = "https://" + process.env.SPACES_BUCKET + "." + process.env.SPACES_CDN + "/" + the_dump;
+    next();
+  });
+  req.pipe(bb);
+};
+
+app.get("/", (req, res) => {
+  res.end('<html><form method="POST" enctype="multipart/form-data" action="/gimme-link"><input type="file" name="file"><input type="submit"></form></html>');
+});
+
+app.post('/gimme-link', maidboy, (req, res) => {
+  res.end(req.url_cute);
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("server go vroom");
 });
